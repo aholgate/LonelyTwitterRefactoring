cf9b3ec813e8992c45a819f10a864cd51eac7a4b
aholgate <aholgate@ualberta.ca>
2017-10-31 17:42:06 -0600

    lab 9

---
+++ app/src/main/java/ca/ualberta/cs/lonelytweet/ImportantLonelyTweet.java
@@ -0,0 +1,8 @@
+package ca.ualberta.cs.lonelytwitter;
+
+/**
+ * Created by Ashley on 2017-10-31.
+ */
+
+public class ImportantLonelyTweet {
+}
---
+++ app/src/main/java/ca/ualberta/cs/lonelytweet/NormalLonelyTweet.java
@@ -0,0 +1,27 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Date;
+
+public class NormalLonelyTweet extends LonelyTweet {
+
+	public NormalLonelyTweet() {
+	}
+
+	public NormalLonelyTweet(String text, Date date) {
+		this.tweetDate = date;
+		this.tweetBody = text;
+	}
+
+	@Override
+	public boolean isValid() {
+		if (tweetBody.trim().length() == 0
+				|| tweetBody.trim().length() > 10) {
+			return false;
+		}
+
+		return true;
+	}
+
+}
